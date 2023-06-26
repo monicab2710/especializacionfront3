50d9b31878f59d464154afbca7902b47e4159663
Mónica Bernal <mok2710@hotmail.com>
2023-06-25 21:37:17 -0500

    deployed and fixing a lot

---
+++ components/Cards/HomeCard.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Link from 'next/link';
+
+export interface HomeCardProps {
+    title: string,
+    image: string,
+    id: number,
+}
+
+export default function HomeCard({ title, image, id }: HomeCardProps) {
+    return (
+        <Card sx={{ minHeight: '490px' }}>
+            <CardMedia
+                component="img"
+                title={`${title} imagen`}
+                height="300"
+                image={image}
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    {title}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                    <Button size="small" disabled>
+                        Comprar
+                    </Button>
+                <Link href={`/comics/${id}`}>
+                    <Button size="small">
+                        Ver detalle
+                    </Button>
+                </Link>
+            </CardActions>
+        </Card>
+    );
+}
